2f8c08f795c80af4f76b505c707c1cb7d02ca8f7
PC-CMONROY\Cmonroyp <carlosmonroy@outlook.com>
2018-07-11 20:50:05 -0500

    cambios home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser'
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private meta: Meta, private title: Title) { }
 
   ngOnInit() {
+
+    this.title.setTitle('Home Page Paises');
+
+    this.meta.addTags([
+      { name: 'author', content: 'Simple Home' },
+      { name: 'keywords', content: 'Angular 6 PWA' },
+      { name: 'description', content: 'Implementacion de buenas practicas!.' },
+    ])
   }
 
 }
